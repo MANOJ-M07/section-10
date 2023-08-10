be8c262c5592bc05c097c688ff52ad807fb6287f
MANOJ-M07 <manojmoorthy01@gmail.com>
2023-08-10 15:37:55 +0530

    initial commit

---
+++ Interface/Interface/Iperson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface Iperson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+    }
+}
